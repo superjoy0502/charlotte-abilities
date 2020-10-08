072c6dafeb517988e636ced550b5b3667ee15ebb
superjoy0502 <superjoy0502@gmail.com>
2020-10-07 20:22:15 -0600

    init 추가 및 WTF

--- abilities/imperfectInvisibility/src/main/kotlin/com/github/superjoy0502/psychic/ability/imperfectinvisibility/TomoriNao.kt
+++ abilities/imperfectInvisibility/src/main/kotlin/com/github/superjoy0502/psychic/ability/imperfectinvisibility/TomoriNao.kt
@@ -1,10 +1,11 @@
 package com.github.superjoy0502.psychic.ability.imperfectinvisibility
 
-import com.github.noonmaru.psychics.Ability
-import com.github.noonmaru.psychics.AbilityConcept
-import com.github.noonmaru.psychics.Esper
-import com.github.noonmaru.psychics.Psychic
+import com.github.noonmaru.psychics.*
+import com.github.noonmaru.psychics.item.isPsychicbound
 import com.github.noonmaru.psychics.plugin.PsychicPlugin
+import com.github.noonmaru.tap.config.Config
+import com.github.noonmaru.tap.config.Name
+import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
@@ -12,10 +13,39 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemFlag
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.Plugin
 
+@Name("imperfectInvisibility")
+class TomoriNaoConcept : AbilityConcept(){
+    init {
+        type = AbilityType.TOGGLE
+        displayName = "불완전 투명화"
+        levelRequirement = 0
+        cooldownTicks = 200
+        cost = 40.0
+        castingTicks = 20
+        durationTicks = 1200
+        supplyItems = listOf(
+            ItemStack(Material.IRON_BARS).apply {
+                val meta = itemMeta
+                meta.setDisplayName("${ChatColor.DARK_PURPLE}${ChatColor.BOLD}투명화")
+                meta.isUnbreakable = true
+                meta.isPsychicbound = true
+                meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE)
+                itemMeta = meta
+            }
+        )
+        description = listOf(
+            "지정된 대상에게서 모습을 감춥니다.",
+            "철괴를 좌클릭하여 대상을 지정할 수 있으며",
+            "우클릭 시 \${imperfectInvisibility.castingTicks / 20.0}초 뒤 \${imperfectInvisibility.durationTicks / 20.0}초 동안 모습을 감춥니다."
+        )
+    }
+}
 
-class TomoriNao : Ability<AbilityConcept>(), Listener{
+class TomoriNao : Ability<TomoriNaoConcept>(), Listener{
 
     // 능력: 지정된 대상에게서 모습을 감춤
 
@@ -31,11 +61,11 @@ class TomoriNao : Ability<AbilityConcept>(), Listener{
     fun onPlayerInteract(event: PlayerInteractEvent) {
         player = event.player
         if (event.action == Action.LEFT_CLICK_AIR && player.inventory.itemInMainHand.type == Material.IRON_BARS) {
-            player.sendMessage("능력 사용자가 철괴를 들고 왼손을 휘둘렀다.")
+            player.sendMessage("능력 사용자가 철괴를 들고 왼손을 휘둘렀다.") // Debug
             for (entity in player.world.entities) {
                 if (entity.location === player.eyeLocation && entity.type == EntityType.PLAYER) {
                     target = entity as Player
-                    player.sendMessage("You're looking at " + target.name + "!")
+                    player.sendMessage("You're looking at " + target.name + "!") // Debug
                 }
             }
         }
@@ -48,5 +78,8 @@ class TomoriNao : Ability<AbilityConcept>(), Listener{
     private fun hideMe(){
         target.hidePlayer(psychic.plugin, player)
     }
+
+    // TODO 지속시간 초과 시 투명화 해제
+    // TODO 피격 시 투명화 해제 및 쿨타임 초기화
 
 }
