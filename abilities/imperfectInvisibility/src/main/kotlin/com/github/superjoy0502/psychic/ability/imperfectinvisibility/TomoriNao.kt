dc5b629d5071e786b63fe266c44c96d7fd21574e
superjoy0502 <superjoy0502@gmail.com>
2020-10-08 01:41:54 -0600

    타겟 설정 기능 완성 (레이캐스트 활용)

--- abilities/imperfectInvisibility/src/main/kotlin/com/github/superjoy0502/psychic/ability/imperfectinvisibility/TomoriNao.kt
+++ abilities/imperfectInvisibility/src/main/kotlin/com/github/superjoy0502/psychic/ability/imperfectinvisibility/TomoriNao.kt
@@ -1,55 +1,68 @@
 package com.github.superjoy0502.psychic.ability.imperfectinvisibility
 
-import com.github.noonmaru.psychics.*
+import com.github.noonmaru.psychics.AbilityConcept
+import com.github.noonmaru.psychics.AbilityType
+import com.github.noonmaru.psychics.ActiveAbility
 import com.github.noonmaru.psychics.item.isPsychicbound
-import com.github.noonmaru.psychics.plugin.PsychicPlugin
+import com.github.noonmaru.psychics.util.TargetFilter
 import com.github.noonmaru.tap.config.Config
 import com.github.noonmaru.tap.config.Name
 import org.bukkit.ChatColor
+import org.bukkit.FluidCollisionMode
 import org.bukkit.Material
+import org.bukkit.entity.Entity
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.Action
-import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.player.PlayerToggleSneakEvent
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
-import org.bukkit.plugin.Plugin
 
 @Name("imperfectInvisibility")
-class TomoriNaoConcept : AbilityConcept(){
+class TomoriNaoConcept : AbilityConcept() {
+
+    @Config
+    var invisibleDuration: Int = 1200
+
+    @Config
+    var invisibleCooldown: Int = 200
+
     init {
         type = AbilityType.TOGGLE
         displayName = "불완전 투명화"
-        levelRequirement = 0
-        cooldownTicks = 200
         cost = 40.0
         castingTicks = 20
-        durationTicks = 1200
         supplyItems = listOf(
-            ItemStack(Material.IRON_BARS).apply {
-                val meta = itemMeta
-                meta.setDisplayName("${ChatColor.DARK_PURPLE}${ChatColor.BOLD}투명화")
-                meta.isUnbreakable = true
-                meta.isPsychicbound = true
-                meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE)
-                itemMeta = meta
-            }
+                ItemStack(Material.GLASS_PANE).apply {
+                    val meta = itemMeta
+                    meta.setDisplayName("${ChatColor.DARK_PURPLE}${ChatColor.BOLD}투명화")
+                    meta.isUnbreakable = true
+                    meta.isPsychicbound = true
+                    meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE)
+                    itemMeta = meta
+                }
         )
+        interruptible = true
+        wand = ItemStack(Material.GLASS_PANE)
         description = listOf(
-            "지정된 대상에게서 모습을 감춥니다.",
-            "철괴를 좌클릭하여 대상을 지정할 수 있습니다."
+                "지정된 대상에게서 모습을 감춥니다.",
+                "웅크려서 대상을 지정할 수 있으며",
+                "철괴 클릭 시 \${common.casting-ticks / 20}초 후 \${imperfectInvisibility.invisible-duration / 20}초 동안 모습을 감춥니다.",
+                "투명화 중 공격을 하면 투명화가 풀리며",
+                "\${imperfectInvisibility.invisible-cooldown / 20}초의 쿨타임을 가집니다."
         )
     }
+
 }
 
-class TomoriNao : Ability<TomoriNaoConcept>(), Listener{
+class TomoriNao : ActiveAbility<TomoriNaoConcept>(), Listener {
 
     // 능력: 지정된 대상에게서 모습을 감춤
 
-    private lateinit var target: Player
-    private lateinit var player: Player
+    private var target: Player? = null
+    private var player: Player? = null
 
     override fun onEnable() {
         psychic.registerEvents(this)
@@ -57,28 +70,60 @@ class TomoriNao : Ability<TomoriNaoConcept>(), Listener{
 
     // 대상 지정
     @EventHandler
-    fun onPlayerInteract(event: PlayerInteractEvent) {
-        player = event.player
-        if (event.action == Action.LEFT_CLICK_AIR && player.inventory.itemInMainHand.type == Material.IRON_BARS) {
-            player.sendMessage("능력 사용자가 철괴를 들고 왼손을 휘둘렀다.") // Debug
-            for (entity in player.world.entities) {
-                if (entity.location === player.eyeLocation && entity.type == EntityType.PLAYER) {
-                    target = entity as Player
-                    player.sendMessage("You're looking at " + target.name + "!") // Debug
-                }
+    fun onPlayerSneak(event: PlayerToggleSneakEvent) {
+        val hit: Entity? = processRayTrace()
+        player = esper.player
+
+        when {
+            hit == null -> {
+                return
+            }
+            hit.type == EntityType.PLAYER -> {
+                target = hit as Player
+                player!!.sendActionBar("${ChatColor.AQUA}${ChatColor.BOLD}Target: ${target!!.name}")
+            }
+            hit.type != EntityType.PLAYER -> {
+                player!!.sendActionBar("${ChatColor.RED}${ChatColor.BOLD}That entity is not a Player!")
             }
         }
-        else if ((event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK) && player.inventory.itemInMainHand.type == Material.IRON_BARS){
-            hideMe()
+    }
+
+    private fun processRayTrace(): Entity? {
+        val from = esper.player.eyeLocation
+        val direction = from.direction
+        from.world.rayTrace(
+                from,
+                direction,
+                64.0,
+                FluidCollisionMode.NEVER,
+                true,
+                1.0,
+                TargetFilter(esper.player)
+        )?.let { rayTraceResult ->
+            rayTraceResult.hitEntity?.let { hit ->
+                if (hit is LivingEntity) {
+                    return hit
+                }
+            }
+
+            return null
         }
+
+        return null
+    }
+
+    override fun onCast(target: Any?) {
+        hideMe()
     }
 
     // 대상에게서 모습 감추기
-    private fun hideMe(){
-        target.hidePlayer(psychic.plugin, player)
+    private fun hideMe() {
+        if (player != null && target != null){
+            player?.let { target?.hidePlayer(psychic.plugin, it) }
+        }
     }
 
-    // TODO 지속시간 초과 시 투명화 해제
-    // TODO 피격 시 투명화 해제 및 쿨타임 초기화
+    // TODO 지속시간 초과 시 투명화 해제 및 쿨타임 초기화
+    // TODO 공격 시 투명화 해제 및 쿨타임 초기화
 
 }
